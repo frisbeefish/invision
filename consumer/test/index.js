3256e70862c85c5d68d2b1d46c5775f7697da5d3
Scott Williams <mantaray@cruzio.com>
2015-09-11 18:05:41 -0700

    added tests for math

--- consumer/test/index.js
+++ consumer/test/index.js
@@ -17,7 +17,6 @@ var after = lab.after;
 var expect = Code.expect;
  
 suite('speed', function () {
-    
     test('returns true if processing time is below one second', function (done) {
 
         var options = {
@@ -53,7 +52,134 @@ suite('speed', function () {
         // Send the GET request to the consumer.
         //
         http.request(options, callback).end();
-
-        
     });
+});
+
+suite('math', function () {
+
+    test('returns true if addition works', function (done) {
+
+        var options = {
+          host: 'localhost',
+          port: "3000",
+          path: '/'
+        };
+
+        options.path = "/?eq=" + encodeURIComponent("2+3=");
+
+        //
+        // This is called once the HTTP GET has returned.
+        //
+        function callback(response) {
+          var str = '';
+
+          response.on('data', function (chunk) {
+            str += chunk;
+          });
+
+          response.on('end', function () {
+            expect(parseInt(str)).to.equal(5);
+            done();
+          });
+        }
+
+        //
+        // Send the GET request to the consumer.
+        //
+        http.request(options, callback).end();
+    });
+
+    test('returns true if multiplication works', function (done) {
+        var options = {
+          host: 'localhost',
+          port: "3000",
+          path: '/'
+        };
+
+        options.path = "/?eq=" + encodeURIComponent("2*3=");
+
+        //
+        // This is called once the HTTP GET has returned.
+        //
+        function callback(response) {
+          var str = '';
+
+          response.on('data', function (chunk) {
+            str += chunk;
+          });
+
+          response.on('end', function () {
+            expect(parseInt(str)).to.equal(6);
+            done();
+          });
+        }
+
+        //
+        // Send the GET request to the consumer.
+        //
+        http.request(options, callback).end();
+    });
+
+    test('returns true if division works', function (done) {
+        var options = {
+          host: 'localhost',
+          port: "3000",
+          path: '/'
+        };
+
+        options.path = "/?eq=" + encodeURIComponent("10/5=");
+
+        //
+        // This is called once the HTTP GET has returned.
+        //
+        function callback(response) {
+          var str = '';
+
+          response.on('data', function (chunk) {
+            str += chunk;
+          });
+
+          response.on('end', function () {
+            expect(parseInt(str)).to.equal(2);
+            done();
+          });
+        }
+
+        //
+        // Send the GET request to the consumer.
+        //
+        http.request(options, callback).end();
+    });
+
+    test('returns true if subtraction works', function (done) {
+        var options = {
+          host: 'localhost',
+          port: "3000",
+          path: '/'
+        };
+
+        options.path = "/?eq=" + encodeURIComponent("10-5=");
+
+        //
+        // This is called once the HTTP GET has returned.
+        //
+        function callback(response) {
+          var str = '';
+
+          response.on('data', function (chunk) {
+            str += chunk;
+          });
+
+          response.on('end', function () {
+            expect(parseInt(str)).to.equal(5);
+            done();
+          });
+        }
+
+        //
+        // Send the GET request to the consumer.
+        //
+        http.request(options, callback).end();
+    });
+
 });
